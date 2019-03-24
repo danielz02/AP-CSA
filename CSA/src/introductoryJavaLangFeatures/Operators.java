7563b03d566516aa71c27561f37343da3321909d
danielz02 <zchdz0215@gmail.com>
2019-03-25 01:48:50 +0800

    Refined Javadoc comments.

--- CSA/src/introductoryJavaLangFeatures/Operators.java
+++ CSA/src/introductoryJavaLangFeatures/Operators.java
@@ -5,11 +5,11 @@ public class Operators {
 	
 	int k;
 	int i = 1;
-	k = 1 + i++;
+	k = 1 + i++;//1 + i will be implemented first.
 	System.out.println(k);
 	
 	i = 1;
-	k = 1 + ++i;
+	k = 1 + ++i;//++ has higher precedence over +
 	System.out.println(k);
 	
 	}
